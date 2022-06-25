71fb398dfd4e2df5eb0e8d5e7c50cd42c9c7315f
kolcer <35612198+kolcer@users.noreply.github.com>
2022-06-25 21:14:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ async def PRINT_TIPS(channel,key):
     tips = list_tips(key)
     combined_string = ""
     for i in range(len(tips)):
-        new_string = combined_string + str(i) + ") " + tips[i] + "\n"
+        new_string = combined_string + str(i) + ") " + str(tips[i]) + "\n"
         if len(new_string) > 2000:
             await SEND(channel,combined_string)
-            combined_string = str(i) + ") " + tips[i] + "\n"
+            combined_string = str(i) + ") " + str(tips[i]) + "\n"
         else:
             combined_string = new_string
             await SEND(channel,combined_string)
