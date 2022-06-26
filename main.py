350eb7fe1496475521d02242c7abdf23e15e9c51
kolcer <35612198+kolcer@users.noreply.github.com>
2022-06-26 12:34:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,13 @@ def delete_tip(key, index):
 def list_tips(key):
     result = db.lrange(key,0,-1)
     return result
-    
 
+def show_random_tip(key)
+    index = random.randint(0,db.llen(key))
+    result = db.lrange(key,index,index)
+    return result[0].decode("utf-8")
+    
+tips[i].decode("utf-8")
 ### PRIVATE ASYNC FUNCTIONS ###
 
 #TODO: implement anti rate-limit measures here
@@ -88,18 +93,37 @@ async def on_message(message):
 
     msg = message.content
     usr = message.author
-    channel = message.channel
+    ch = message.channel
     
     ## user must not be a bot
     if usr.bot == True:
         return
+    
+    #this will avoid old activatig with old bot
+    if msg.startswith(">"):
+        return
+    
+    #normal non-admin usage
+    if not msg.startswith("|"):
         
+        lmsg = msg.lowercase()
+        
+        ## tips/tricks trigger
+        split = lmsg.split(" ", 1)
+        if split[1] == "tip" or split[1] == "trick":
+            if split[0] in TIPS_KEYS:
+                SEND(ch,show_random_tip(split[0]))
+        elif split[1] == "trivia":
+            if split[0] in TIPS_KEYS:
+                key = split[0] + "T"
+                SEND(ch,show_random_tip(key))
+                
     ## tips/tricks admin command
-    if msg.startswith("|"):
+    else:
 
         #check for admin
         if not usr.id in ADMINS:
-            await SEND(channel,"You are not allowed to use this command.")
+            await SEND(ch,"You are not allowed to use this command.")
             return
             
         #deterimine the key (this is an alignment name in most cases)
@@ -107,7 +131,7 @@ async def on_message(message):
         key = split[1]
         
         if not key in TIPS_KEYS:
-            await SEND(channel,"Invalid alignment.")
+            await SEND(ch,"Invalid alignment.")
             return
            
         #tip or trick?
@@ -117,27 +141,28 @@ async def on_message(message):
             #for trivia, key has extra "T" at the end
             key = key + "T"
         elif not msg.startswith("tip",2):
-            await SEND(channel,"Invalid command.")
+            await SEND(ch,"Invalid command.")
             return
         
         #add tip   
         if msg.startswith("n",1):
-           add_tip(key,split[2])
-           await SEND(channel,"New " + split[1] + " " + tot + "  added.")
-           return
+            add_tip(key,split[2])
+            await SEND(ch,"New " + split[1] + " " + tot + "  added.")
+            return
 
         #list tips
         if msg.startswith("l",1):
-           await SEND(channel,split[1] + " " + tot + "(s):")
-           await PRINT_TIPS(channel, key)
-           return
+            await SEND(ch,split[1] + " " + tot + "(s):")
+            await PRINT_TIPS(ch, key)
+            return
             
         #delete tip
         if msg.startswith("d",1):
-           delete_tip(key,int(split[2]))
-           await SEND(channel,split[1] + " " + tot + "(s):")
-           await PRINT_TIPS(channel, key)
-           return
-           
+            delete_tip(key,int(split[2]))
+            await SEND(ch,split[1] + " " + tot + "(s):")
+            await PRINT_TIPS(ch, key)
+            return
+    
+         
 #run the bot 
 client.run(os.environ['TOKEN'])
