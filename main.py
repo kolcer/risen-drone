b59f06241bafacd0705748444ae21ab00d23e943
Nickplus <alvin3906@gmail.com>
2022-07-16 00:20:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1770,30 +1770,28 @@ async def on_message(message):
                
     ## admin command
     else:
-        try:
-            #check for admin
-            if not ADMIN in usr.roles:
-                await SEND(ch,"You are not allowed to use this command.")
-                return
 
-            #deterimine the key (this is an alignment name in most cases)
-            split = msg.split(" ", 2)
-    
-            #give ckr
-            if msg.startswith("ckr to ", 1):
-                for mem in SERVER.members:
-                    if mem.name.lower() + "#" + mem.discriminator == split[2]:
-                        await ADD_ROLES(mem, CKR)
-                        break
-                return  
-        except Exception as ex:
-            await SEND(CHANNELS["bot-testing"], ex)
+        #check for admin
+        if not ADMIN in usr.roles:
+            await SEND(ch,"You are not allowed to use this command.")
+            return
+
+        #deterimine the key (this is an alignment name in most cases)
+        split = msg.split(" ", 2)
+   
+        #give ckr
+        if msg.startswith("ckr to ", 1):
+            for mem in SERVER.members:
+               if mem.name.lower() + "#" + mem.discriminator == split[2]:
+                   await ADD_ROLES(mem,CKR)
+                   break
+            return  
 
         #remove ckr
         if msg.startswith("ckr from ", 1):
             for mem in SERVER.members:
                if mem.name.lower() + "#" + mem.discriminator == split[2]:
-                   await REMOVE_ROLES(mem, CKR)
+                   await REMOVE_ROLES(mem,CKR)
                    break
             return   
 
