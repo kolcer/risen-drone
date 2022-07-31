f235413c72ffe97f70ed28aee3bbffeaa513afd3
Nickplus <alvin3906@gmail.com>
2022-07-31 19:41:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1849,6 +1849,10 @@ async def on_message(message):
     ## admin command
     else:
 
+        #check if user is trying to use the spoiler format first
+        if msg.startswith("||"):
+            return
+    
         #check for admin
         if not ADMIN in usr.roles:
             await SEND(ch,"You are not allowed to use this command.")
