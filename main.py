b157a86d56d59a76db718c64167afc7d567e57a7
Nickplus <alvin3906@gmail.com>
2022-07-31 19:23:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1347,7 +1347,7 @@ async def on_message(message):
     eligible = 0
     rolename = ""
 
-    if randomchance != 0 and not FUN_ROLES["Sanctuary Discoverer"] in usr.roles and ch == CHANNELS["bot-commands"]:
+    if randomchance != 0 and not FUN_ROLES["Sanctuary Discoverer"] in usr.roles and ch == CHANNELS["bot-testing"]:
         for role in usr.roles:
             if role.name.lower() in SANCTUARY:
                 eligible = eligible + 1                
