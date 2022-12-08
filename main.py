8a4a9a0b25eb89d53350ec82485a266a8e03d766
kolcer <35612198+kolcer@users.noreply.github.com>
2022-12-08 10:28:25 +0000

    Revert "First try to split the file, moved role management (mostly) to the new file."
    This reverts commit a22155c3381f5fa840c77cf34f6a2aa816eb19ee.

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from datetime import date
 import time
 from difflib import SequenceMatcher
 
-from roles import *
-
 ## CONSTANTS ####
 
+#chatt killer requires 2 hours of inactivity(in seconds)
+CHAT_KILLER_WAIT = 7200
 #player that reaches this level first will win the mini game
 MINI_GAME_TOP_LEVEL = 21
 MINI_GAME_MAX_WAIT = 30
@@ -27,6 +27,7 @@ BROKEN_DRONE_NICK = "BROKEN DRONE"
 #special roles
 #roles that bot can assing to, but not by a regular user commannd
 #values replaced by roles on startup
+CKR = 951424560685805588
 POSSESSED = 988572669521842197
 MURDURATOR = "Murdurators"
 CLIMBER = "Climbers"
@@ -42,7 +43,29 @@ CHANNELS = {
     "bot-testing": 813882658156838923,
 }
 
-
+#worst guns ever made for the gun role
+WORST_GUNS = [
+    "Cochran Turret Revolver",
+    "Chauchat",
+    "Nambu Type 94 Pistol",
+    "Krummlauf",
+    "2 mm Kolibri",
+    "Glisenti Model 1910",
+    "Davy Crockett",
+    "Northover Projector",
+    "Duck's Foot Pistol",
+    "Puckle Gun",
+    "Nock Volley Gun",
+    "Grossflammenwerfer",
+    "Gyrojet",
+    "FP-45 Liberator"
+    "Ross Rifle",
+    "Arsenal AF2011-A1",
+    "CZ-38",
+    "LeMat Revolver",
+    "Boys Anti-Tank Rifle",
+    "No gun name for you",
+]
 
 #this is for tips and trivia database
 TIPS_KEYS = [
@@ -51,11 +74,173 @@ TIPS_KEYS = [
     "possessed", "architect",
 ]
 
+#this roles can be assigned via a morph to command
+#at 0 index we will put a role object during the login proccess.
+#make sure indexes match role name in the server!
+MORPHABLE_ROLES = {
+    "Patron": [ 
+        None,
+        "Go help those noobs, you are now a Patron!",
+        "What about protecting the noobs? Without a Patron around they will be lost.",
+        "You are already assisting the noobs, why would you help them twice?",
+        "Nobody has seen you around lately, what are you doing?",
+    ],
+    "Joker": [ 
+        None,
+        "As if there weren't enough clowns here, you are now a Joker!",
+        "Did you run out of jokes? The Joker guild will hear about this.",
+        "Nice joke.",
+        "You were not a Joker, but you are a Clown now for sure.",
+    ],
+    "Wicked": [ 
+        None,
+        "Unleash all your wickedness, you are now a Wicked!",
+        "You destroyed everything and left nothing behind. Thank you for your services, a Wicked is not needed anymore.",
+        "You have been destroying the stairs until now...",
+        "You are not a Wicked...",
+    ],
+    "Spectre": [ 
+        None,
+        "Spectre guild quote's founder has been identified, you are now a Spectre.",
+        "Once again, Spectre's Founder went MIA.",
+        "We have already found you, or are you planning to leave?",
+        "Your soul didn't belong here to begin with.",
+    ],
+    "Keeper": [ 
+        None,
+        "The staircase is now under your supervision, you successfully became a Keeper.",
+        "You failed to take care of the stairs, and so you are no longer a Keeper.",
+        "Do you need help cleaning the stairs?",
+        "The stairs do not recognise you anyway.",
+    ],
+    "Muggle": [ 
+        None,
+        "Work smarter, not harder. You are now a Muggle!",
+        "The tower was too overwhelming for a weakling like you. Your Muggle license has been revoked.",
+        "Are you having an identity crisis?",
+        "Once a weakling, always a weakling.",
+    ],
+    "Chameleon": [ 
+        None,
+        "Do not let them know your next move, you are now a Chameleon!",
+        "You had many options, yet you came back. You do not get to be a Chameleon anymore.",
+        "You cannot morph to Chameleon via Chameleon, duh.",
+        "I know a Chameleon when I see one, and you aren't one.",
+    ],
+    "Hacker": [ 
+        None,
+        "Welcome to the backdoor, you are now a Hacker!",
+        "You tried to execute some code but as a result you accidentally removed your Hacker permissions.",
+        "You are already inside...",
+        "You do not seem to be a cheater, no reason to remove your permissions.",
+    ],
+    "Thief": [ 
+        None,
+        "Is it really called borrowing? You are now a Thief!",
+        "You actually gave me back the role? How generous. But also that doesn't make you a Thief anymore.",
+        "The thief guild has already hired you.",
+        "The thief guild has never seen you before.",
+     ],
+    "Archon": [ 
+        None,
+        "Typo fixed, happy? You are now an Archon!",
+        "Traveling between portals has been fun, but fun eventually comes to an end. You are no longer an Archon.",
+        "I have already corrected the typo.",
+        "Do you want the fun to end twice?",
+     ],
+     "Drifter": [ 
+        None,
+        "You took the elevator and rose to the top. You are now a Drifter.",
+        "I saw you taking the stairs, you are no longer a Drifter.",
+        "Isn't that you traveling on the platforms?",
+        "You are still taking the stairs.",
+     ],
+     "Heretic": [ 
+        None,
+        "We have banned dark magic, but you do not seem to care. You successfully became a Heretic.",
+        "The circle has made their decision. You are permanently banned from being a Heretic ever again.",
+        "You cared enough to join again?",
+        "You are banned from being a Heretic ever again.",
+    ],
+     "Guns": [ 
+        None,
+        "smh, FINE!",
+        "Finally you came to your senses.",
+        "SMH! FINE!",
+        "You are not a gun.",
+     ],
+}
 
+#not morphable roles
+# 0 - role itself
+# 1 - doesnt have role, and wants it
+# 2 - has role, and wants it
+# 3 - doesnt have role, and wants to remove it
+# 4 - has role, and wants to remove it
+SPECIAL_ROLES = {
+    "Admin": [
+        None,
+        "You are now an Admin! ...Wait, what?",
+        "How funny!",
+        "You are no longer an Admin... You never were.",
+        "I believe you could just go and do it yourself.",
+    ],
+    "Architect": [
+        None,
+        "You should boost the server if you crave for that role.",
+        "You are already an Architect, smh.",
+        "You are not an Architect...",
+        "Just wait for the boost to expire.",
+    ],
+    "Climber": [
+        None,
+        "Please verify to become a climber.",
+        "To the tower you go!",
+        "You are no longer a Climber. Goodbye.",
+        "You are no longer a Climber. Goodbye.",
+    ],
+    "Possessed": [
+        None,
+        "Wait for someone to cast a Heretic Rig.",
+        "You are already possessed...",
+        "You are not possessed...",
+        "Ask someone for mana.",
+    ],
+    #multiple words (ultimate chat killer) would break the script logic
+    "Ultimate": [
+        None,
+        "Your message needs to be last for 2 hours in the <#624227331720085536> channel.",
+        "You have already killed the chat.",
+        "You were not a chat killer in the first place.",
+        "There was an attempt.",
+    ],
 
+}
 
+FUN_ROLES = {
+    "Sanctuary Discoverer": None,
+    "Splicer": None,
+    "Heretic Defier": None,
+    "Architect Design": None,
+    "Pranked the Creator": None,
+    "I was there": None,
+}
 
-
+#pingable roles, no custom messages
+#roles will be fetched on bot startup
+PING_ROLES = {
+    "Announcements":
+        None,
+    "Events":
+        None,
+    "Polls":
+        None,
+    "Updates":
+        None,
+    "Minigames":
+        None,
+}
+IMMUNITY_ROLES = ["Admin", "Murdurators", "Sleazel"]
 
 #bot will react to the webhook emoji, if it finds in a webhook message
 #values will be replaced by emoji objects during startup
@@ -449,6 +634,7 @@ MG_SPELLS = [
 
 # for chat killer role, time when the last message was sent
 # (seconds from unix epoch)
+Last = 0
 rigCaster = None
 ghostMsg = ""
 thirdkill = None
@@ -844,7 +1030,10 @@ def GET_CHANNEL(id):
 def GET_EMOJI(id):
     return client.get_emoji(id)
 
-
+#get up to date ckr members
+def UPDATE_CKR():
+    global CKR
+    CKR = SERVER.get_role(CKR.id)
 
 ### PRIVATE ASYNC FUNCTIONS ###
 async def SEND(channel,message):
@@ -853,13 +1042,22 @@ async def SEND(channel,message):
         return
     return await channel.send(message)
 
-
+#add roles
+async def ADD_ROLES(usr,roles):
+    await usr.add_roles(roles)
+    
+#remove roles
+async def REMOVE_ROLES(usr,roles):
+    await usr.remove_roles(roles)
 
 #purge roles
 async def PURGE_ROLES(role):
     await role.delete()
 
-
+#edit nick
+async def EDIT_NICK(usr,new_nick):
+    if usr.id != 481893862864846861:
+        await usr.edit(nick=new_nick)
     
 #add reaction    
 async def ADD_REACTION(msg,reaction):
@@ -873,6 +1071,8 @@ async def DELETE(message):
   await asyncio.sleep(2)
   await message.delete()
 
+async def EDIT_ROLE(targetrole, newname, motivation):
+  await targetrole.edit(name = newname, reason = motivation)
 
 async def NEW_ROLE(colorpick, rolename):
   return await SERVER.create_role(name = rolename, colour = discord.Colour(int(colorpick, 16)))
@@ -934,6 +1134,9 @@ async def WAIT_FOR_CHAT_KILLER(msg):
             await SEND(CHANNELS["general"],msg.author.mention + " what have you done to this chat.")
             await asyncio.sleep(1)
 
+            if CKR.name != "Definitive Ultimate Chat Killer":
+                await EDIT_ROLE(CKR, "Definitive Ultimate Chat Killer", "This user has killed the chat twice in a row.")
+            return
                 
 
 
@@ -1145,9 +1348,47 @@ async def on_ready():
     global MURDURATOR
     global CLIMBER
     global ADMIN
-
-    PrepareRoles(SERVER.roles)
-
+    for role in SERVER.roles:
+        #morphable
+        if role.name in MORPHABLE_ROLES:
+            MORPHABLE_ROLES[role.name][0] = role
+            continue
+        #ping roles
+        if role.name in PING_ROLES:
+            PING_ROLES[role.name] = role
+            continue
+        if role.name in SPECIAL_ROLES:
+            SPECIAL_ROLES[role.name][0] = role
+            continue
+        #chat killer
+        if role.id == CKR:
+            CKR = role
+            SPECIAL_ROLES["Ultimate"][0] = role
+            continue
+        #possessed (for the rig)
+        if role.id == POSSESSED:
+            POSSESSED = role
+            SPECIAL_ROLES["Possessed"][0] = role
+            continue
+        #climber
+        if role.name == CLIMBER:
+            CLIMBER = role
+            SPECIAL_ROLES["Climber"][0] = role
+        #architect
+        if role.name == "Architect (Booster)":
+            SPECIAL_ROLES["Architect"][0] = role
+            continue
+        #murdurator
+        if role.name == MURDURATOR:
+            MURDURATOR = role
+        #drone tips/tricks admins
+        if role.id == ADMIN:
+            ADMIN = role
+        #fun roles
+        if role.name in FUN_ROLES:
+            FUN_ROLES[role.name] = role
+            continue
+            
     #prepare emojis reactions
     for i, v in EMOJIS_TO_REACT.items():
         EMOJIS_TO_REACT[i] = GET_EMOJI(v)
@@ -1842,20 +2083,72 @@ async def on_message(message):
 
         #morph command
         if lmsg.startswith("morph to"):
-            await SEND(ch,await MorphTo(usr,lsplit[2].capitalize()))
+            role = lsplit[2].capitalize()
+            if role == "Gun":
+                role = "Guns"
 
+            if role in MORPHABLE_ROLES:
+                if role == "Guns":
+                    await EDIT_NICK(usr,random.choice(WORST_GUNS))
+
+                if MORPHABLE_ROLES[role][0] in usr.roles:
+                    await SEND(ch,MORPHABLE_ROLES[role][3])
+                else:
+                    await SEND(ch,MORPHABLE_ROLES[role][1])
+                    await ADD_ROLES(usr,MORPHABLE_ROLES[role][0])
+                return
+
+            if role in SPECIAL_ROLES:
+                if SPECIAL_ROLES[role][0] in usr.roles:
+                    await SEND(ch,SPECIAL_ROLES[role][2])
+                else:
+                    await SEND(ch,SPECIAL_ROLES[role][1])
+                return
+        
         #demorph command (accepts demorph, unmorph and any **morph from combination)
         if lmsg.startswith("morph from",2):
-            await SEND(ch,await DemorphFrom(usr,lsplit[2].capitalize()))
+            role = lsplit[2].capitalize()
+            if role == "Gun":
+                role = "Guns"
+                
+            # if role in MORPHABLE_ROLES:
+            #     await SEND(ch,MORPHABLE_ROLES[role][2])
+            #     await REMOVE_ROLES(usr,MORPHABLE_ROLES[role][0])
+            #     return
 
-        #sub command       
+            if role in MORPHABLE_ROLES:
+                if MORPHABLE_ROLES[role][0] not in usr.roles:
+                    await SEND(ch,MORPHABLE_ROLES[role][4])
+                else:
+                    await SEND(ch,MORPHABLE_ROLES[role][2])
+                    await REMOVE_ROLES(usr,MORPHABLE_ROLES[role][0])
+                return
+                
+            if role in SPECIAL_ROLES:
+                if SPECIAL_ROLES[role][0] in usr.roles:
+                    await SEND(ch,SPECIAL_ROLES[role][4])
+                else:
+                    await SEND(ch,SPECIAL_ROLES[role][3])
+                return
+        
+        #sub command
         if lmsg.startswith("sub to"):
-            await SEND(ch,await SubTo(usr,lsplit[2].capitalize()))
+            role = lsplit[2].capitalize()
+            if role in PING_ROLES:
+                await SEND(ch,"You have subscribed to " + role + "!")
+                await ADD_ROLES(usr,PING_ROLES[role])
+                return
 
-        #unsub command
+        #unsub command (aceppts unsub, desub and any **sub from combination)
         if lmsg.startswith("sub from",2):
-            await SEND(ch,await UnsubFrom(usr,lsplit[2].capitalize()))
-       
+            role = lsplit[2].capitalize()
+            if role in PING_ROLES:
+                await SEND(ch,"You have unsubscribed from " + role + "!")
+                await REMOVE_ROLES(usr,PING_ROLES[role])
+                return
+                
+        
+        
         ## tips/tricks trigger
         if len(lsplit) == 2:
             if lsplit[1] == "tip" or lsplit[1] == "trick":
