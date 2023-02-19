8baf4e08d7ce0a863103bebbf60188e4faecc22d
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-02-19 15:22:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ async def on_message(message):
     usr = message.author
     ch = message.channel
 
-    if ch.id == 845454640103424032 and (not message.attachments and 'http' not in msg):
+    if ch.id == 845454640103424032 and (not message.attachments and 'http' not in msg) and not usr.id == 827952429290618943:
         for role in usr.roles:
             if role.name in IMMUNITY_ROLES:
                 return
