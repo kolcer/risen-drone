15c1e000a9fd32b46e18727669fc1a792da516c7
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-04-07 13:48:59 +0200

    making progress

--- fighting.py
+++ fighting.py
@@ -19,31 +19,31 @@ def FG_NEXT_PLAYER():
     if FG['currentPlayer'] > len(FG_QUEUE) - 1:
         FG['currentPlayer'] = 0
 
-# class selection
+
 async def FightingProcessClass(usr, msg):
     lmsg = msg.lower()
-    print("here")
-    print(FG['status'] )
         
     if FG['status'] == "class-picking":
-        print("here")
-        # alignment not added yet
+
         if lmsg in SANCTUARY.keys() and lmsg not in FG_CLASSES.keys():
             await SEND(FG["channel"], "The selected Alignment has not made it into the fighting scene yet, sadly.") 
             return
 
-        # the chosen alignment is not in alignments
         if lmsg not in SANCTUARY.keys() and lmsg not in FG_CLASSES.keys():
             await SEND(FG["channel"], "I gave you a list. Read it and answer accordingly!") 
             return
 
-        # player picks class
-        FG_PLAYERS[usr] = [lmsg, None, 70] # Class -> status -> accuracy
+        if FG_PLAYERS[usr][0] != None:
+            await SEND(FG["channel"], "No takes-back.") 
+            return
+
+        FG_PLAYERS[usr] = [lmsg, None, 70] # class, status, accuracy
         await SEND(FG["channel"], f"{usr.mention} is playing as {lmsg.capitalize()}.")
+
         FG["class-picked"] += 1
 
-        # both players picked a class
         if FG["class-picked"] == 2:
+            await asyncio.sleep(2)
             FG['status'] = "battling"
             toSend = "Everyone is now ready. Here are your picks:\n\n"
 
@@ -53,18 +53,9 @@ async def FightingProcessClass(usr, msg):
             toSend += "\nLet's begin."
 
             await SEND(FG["channel"], toSend)
-            return # sure you already knew this but move it to the right when you uncomment the thing blow
-            # :skull:
+        return
 
-    # pretty sure this needs to be tabbed forward :skull:    
-    # elif FG['status'] == "on" and lmsg in MG_SPELLS and MG_QUEUE[FG['currentPlayer']] == usr:
-    #     spell = lmsg
-    #     while spell == "chameleon":
-    #         spell = random.choice(MG_SPELLS)
-    #     MG_NEXT_PLAYER()
-    #     await MG_LOOP(MG_ACTION(usr,spell))
 
-# read the function name lol
 async def PlayFightingGame(usr, ch):
     if FG['status'] != "off":
         await SEND(ch, "A match is already in progress.")
@@ -84,26 +75,33 @@ async def PlayFightingGame(usr, ch):
             FG_RESET()
         return
 
-# adds player to FG_PLAYERS dictionary - join game function .-.
+
 async def JoinFightingGame(usr):
     if usr in FG_PLAYERS:
         await SEND(FG['channel'], "Wait for someone else.")
         return
     else:
-        FG['status'] = "class-picking"
         FG_PLAYERS[usr] = [None, None, 70] #class, status, accuracy
         FG_QUEUE.append(usr)
         await SEND(FG["channel"], f"{usr.mention} is eager to fight too.\n")
         await asyncio.sleep(2)
         await SEND(FG["channel"], ClassShowcase())
+        FG['status'] = "class-picking"
+
+        await asyncio.sleep(30)
+        for user in FG_PLAYERS:
+            if FG_PLAYERS[user][0] == None:
+                await SEND(FG["channel"], "One or more users have not selected an Alignment in the given time. Fight is cancelled.")
+                FG_RESET()
+                break
         return
 
-# shows all the alignments in a message
+
 def ClassShowcase():
     toSend = "You may now pick your Alignment. Select between:\n\n"
 
     for alignment in FG_CLASSES.keys():
-        toSend += f"`{str(alignment.capitalize())}`\n" # fstrings are superior
+        toSend += f"`{str(alignment.capitalize())}`\n" 
         
     return toSend
 
