6276c95afbd6313787d7b484d8c15f0698cce2e6
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-04-08 23:58:32 +0200

    maybe it works now

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ async def on_reaction_add(reaction, user):
     await SplicerRig(reaction,user)
     print(reaction.emoji)
 
-    if (reaction.emoji == "<:csSleazelApproves:791393163343560715>" or reaction.emoji == "<:csSleazelNotApproved:1038172235170578532>") and user.id != 481893862864846861:
+    if (str(reaction.emoji) == "<:csSleazelApproves:791393163343560715>" or str(reaction.emoji) == "<:csSleazelNotApproved:1038172235170578532>") and user.id != 481893862864846861:
         await reaction.remove(user)
 
 @client.event
