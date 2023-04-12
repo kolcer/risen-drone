029d9866462a88ca96bd057d352ca927934713c4
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-04-12 23:52:48 +0200

    moved the message back in bot-commands

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ async def add_entry_with_check(key, new_entry):
             break
 
     if roleCounter == 0:
-        await SEND(CHANNELS["bot-testing"], f"{new_entry.name}, you just found your first secret role. Type `bd show profile` to view it.")
+        await SEND(CHANNELS["bot-commands"], f"{new_entry.name}, you just found your first secret role. Type `bd show profile` to view it.")
 
     roleCounter = 0
 
