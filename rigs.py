8b3985f94ece22a938a01833f01e4dda0098c5ae
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-04-14 16:07:44 +0200

    cast again v7

--- rigs.py
+++ rigs.py
@@ -29,6 +29,12 @@ class CastAgain(discord.ui.View):
             usr = interaction.user
             if usr == self.caster:
                 await Rig(self.type, self.channel, self.caster)
+
+                #this is not optimal but otherwise it will give an error as this button doesn't give a response. this is an empty response.
+                try:
+                    interaction.respond()
+                except:
+                    pass
                 self.stop()
             elif usr != self.caster:
                 await INTERACTION(interaction.response, "You did not cast this rig.", True)
