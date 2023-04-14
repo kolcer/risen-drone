a4d63077c71cb3553e775886d05372d857da1a5d
Rolo <104144029+Nicksmth@users.noreply.github.com>
2023-04-14 12:35:15 +0200

    created a new file "views.py".
    so that we can add more views without issues

--- rigs.py
+++ rigs.py
@@ -1,6 +1,7 @@
 import time
 
 from globals import *
+from views import *
 from rated import *
 from roles import *
 from globals import *
@@ -17,41 +18,6 @@ def disableSplicer():
     SPLICER_RIG["reactionmessage"] = None
     SPLICER_RIG["user-name"] = ""
     SPLICER_RIG['rigcaster-name'] = ""
-
-class SplicerView(discord.ui.View):
-    async def tooLate(self):
-        for item in self.children:
-            item.disabled = True
-
-        disableSplicer()
-
-        await EDIT_VIEW_MESSAGE(self.message, self.message.content, self)
-
-    async def on_timeout(self) -> None:
-        await self.tooLate()
-        await SEND(self.message.channel, "Too little, too late.")
-
-    @discord.ui.button(label="Refuse", custom_id = "SpliceNameNo", style = discord.ButtonStyle.red)
-    async def declined(self, interaction: discord.Interaction, button: discord.ui.Button):
-        usr = interaction.user
-        if usr == SPLICER_RIG["user"] and SPLICER_RIG["active"] and self.message == SPLICER_RIG["reactionmessage"]:
-            disableSplicer()
-            
-            await interaction.response.send_message("Splice request declined. That's too bad.")
-            self.stop()
-
-    @discord.ui.button(label="Accept your fate", custom_id = "SpliceNameYes", style = discord.ButtonStyle.green)
-    async def accepted(self, interaction: discord.Interaction, button: discord.ui.Button):
-        usr = interaction.user
-        if usr == SPLICER_RIG["user"] and SPLICER_RIG["active"] and self.message == SPLICER_RIG["reactionmessage"]:
-            await EDIT_NICK(usr, SPLICER_RIG["user-name"])
-            await asyncio.sleep(1)
-            await EDIT_NICK(RIG_DATA['rigCaster'], SPLICER_RIG['rigcaster-name'])
-
-            disableSplicer()
-
-            await interaction.response.send_message("Splice request accepted. Enjoy your new display names.")
-            self.stop()
 
 
 def rigImmunity(usr1, usr2):
@@ -477,7 +443,6 @@ async def ExecuteSplicerRig(ch,usr):
 
 
     view = SplicerView(timeout = 60)
-    # await ch.send(view=view)
     message = await SEND_VIEW(ch, toSend, view)
     view.message = message
     SPLICER_RIG["reactionmessage"] = message
---
+++ views.py
@@ -0,0 +1,43 @@
+import time
+
+from globals import *
+from rigs import disableSplicer
+from rated import *
+from roles import *
+from globals import *
+from discord.ext import commands
+
+class SplicerView(discord.ui.View):
+    async def tooLate(self):
+        for item in self.children:
+            item.disabled = True
+
+        disableSplicer()
+
+        await EDIT_VIEW_MESSAGE(self.message, self.message.content, self)
+
+    async def on_timeout(self) -> None:
+        await self.tooLate()
+        await SEND(self.message.channel, "Too little, too late.")
+
+    @discord.ui.button(label="Refuse", custom_id = "SpliceNameNo", style = discord.ButtonStyle.red)
+    async def declined(self, interaction: discord.Interaction, button: discord.ui.Button):
+        usr = interaction.user
+        if usr == SPLICER_RIG["user"] and SPLICER_RIG["active"] and self.message == SPLICER_RIG["reactionmessage"]:
+            disableSplicer()
+            
+            await interaction.response.send_message("Splice request declined. That's too bad.")
+            self.stop()
+
+    @discord.ui.button(label="Accept your fate", custom_id = "SpliceNameYes", style = discord.ButtonStyle.green)
+    async def accepted(self, interaction: discord.Interaction, button: discord.ui.Button):
+        usr = interaction.user
+        if usr == SPLICER_RIG["user"] and SPLICER_RIG["active"] and self.message == SPLICER_RIG["reactionmessage"]:
+            await EDIT_NICK(usr, SPLICER_RIG["user-name"])
+            await asyncio.sleep(1)
+            await EDIT_NICK(RIG_DATA['rigCaster'], SPLICER_RIG['rigcaster-name'])
+
+            disableSplicer()
+
+            await interaction.response.send_message("Splice request accepted. Enjoy your new display names.")
+            self.stop()
