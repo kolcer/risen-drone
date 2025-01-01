1ac05251eff3992ed4f163fe8aa3a49f1da82403
Rolo <alvin3906@gmail.com>
2025-01-01 17:16:40 +0100

    Fixed per sleazel request

--- ladders.py
+++ ladders.py
@@ -66,7 +66,7 @@ def MG_ACTION(plr, action):
                 MG_PLAYERS[plr] -= 1
                 toSend += " failed a stairjump, made the drones feel bad for them, and caused them to add 1 floor to the tower!"
             else:
-                MG_PLAYERS["topLevel"] += 2
+                LADDERS["topLevel"] += 2
                 MG_PLAYERS[plr] += 1
                 toSend += " stairjumped and made the drones to add 2 floors to the tower!"
                 
