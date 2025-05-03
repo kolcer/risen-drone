d58c7f8059e8db4064282ac09dee306922bfb9fb
Rolo <alvin3906@gmail.com>
2025-05-03 16:48:06 +0200

    should work for all 4 pages

--- main.py
+++ main.py
@@ -901,35 +901,31 @@ async def on_message(message):
 
             # Prepare total climbs for each alignment in PAGE 4 -- good luck sleazel
             user_stats = {k.decode("utf-8"): v.decode("utf-8") for k, v in get_user_stats(target).items()}
-            print(user_stats)
 
             user_climbs = ""
             for alignment in RIG_LIST:
-                value = user_stats.get(f"{alignment.upper()}_climbs", "N/A") 
-                print(value)
-
-                user_climbs += f'**{alignment.capitalize()}** total climbs:\n{value}\n\n'
+                user_climbs += f'**{alignment.capitalize()}** total climbs:\n{user_stats.get(f"{alignment.upper()}_climbs", "N/A")}\n\n'
 
             view.data[3] = user_climbs
 
             # Prepare best times for each alignment in Classic Tower in PAGE 5 -- good luck sleazel
             user_times = ""
             for alignment in RIG_LIST:
-                user_times += f'**{alignment.capitalize()}** best time:\nN/Ams\n\n'
+                user_times += f'**{alignment.capitalize()}** best time:\n{user_stats.get(f"{alignment.upper()}_classic", "N/A")}ms\n\n'
 
             view.data[4] = user_times
 
             # Prepare best times for each alignment in Pro Tower in PAGE 6 -- good luck sleazel
             user_times = ""
             for alignment in RIG_LIST:
-                user_times += f'**{alignment.capitalize()}** best time:\nN/Ams\n\n'
+                user_times += f'**{alignment.capitalize()}** best time:\n{user_stats.get(f"{alignment.upper()}_pro", "N/A")}ms\n\n'
 
             view.data[5] = user_times
 
             # Prepare best times for each alignment in Infinite Tower in PAGE 7 -- good luck sleazel
             user_times = ""
             for alignment in RIG_LIST:
-                user_times += f'**{alignment.capitalize()}** best time:\nN/Ams\n\n'
+                user_times += f'**{alignment.capitalize()}** best time:\n{user_stats.get(f"{alignment.upper()}_infinite", "N/A")}ms\n\n'
 
             view.data[6] = user_times
 
