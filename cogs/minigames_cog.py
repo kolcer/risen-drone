cd2fdba02ddd3f63c27c85aef5e46805faabfd37
Rolo <johndoe@example.com>
2026-03-17 14:02:17 +0100

    fixed checks

--- cogs/minigames_cog.py
+++ cogs/minigames_cog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from globals import QUIZ, QUIZZERS
 from rated import DEFER, FOLLOWUP, INTERACTION
 from quiz import StartQuiz, JoinQuiz
 
@@ -13,14 +14,17 @@ class MinigamesCog(commands.Cog):
         if interaction.guild is None or interaction.channel is None:
             await INTERACTION(interaction, "Use this command in the Crazy Stairs server!", True)
             return
+        
+        if not QUIZ["active"] and not QUIZ["second-player"]:
+            await DEFER(interaction)
 
-        await DEFER(interaction)
-
-        try:
-            await StartQuiz(interaction.user, interaction.channel, interaction)
-        except Exception as exc:
-            await FOLLOWUP(f"Something went wrong with `/start_quiz`: {exc}", interaction)
-            raise
+            try:
+                await StartQuiz(interaction.user, interaction.channel, interaction)
+            except Exception as exc:
+                await FOLLOWUP(f"Something went wrong with `/start_quiz`: {exc}", interaction)
+                raise
+        else:
+            await FOLLOWUP("A quiz is already in progress! Type 'join quiz' to participate.", interaction)
 
     @discord.app_commands.command(name="quiz_join", description="Join an in-progress Crazy Stairs knowledge quiz (former 'join quiz')")
     async def join_quiz(self, interaction: discord.Interaction):
@@ -28,10 +32,13 @@ class MinigamesCog(commands.Cog):
             await INTERACTION(interaction, "Use this command in the Crazy Stairs server!", True)
             return
 
-        await DEFER(interaction)
+        if QUIZ["second-player"] and interaction.user not in QUIZZERS:
+            await DEFER(interaction)
 
-        try:
-            await JoinQuiz(interaction.user, interaction.channel)
-        except Exception as exc:
-            await FOLLOWUP(f"Something went wrong with `/join_quiz`: {exc}", interaction)
-            raise
+            try:
+                await JoinQuiz(interaction.user, interaction.channel)
+            except Exception as exc:
+                await FOLLOWUP(f"Something went wrong with `/join_quiz`: {exc}", interaction)
+                raise
+        else:
+            await FOLLOWUP("No quiz is currently looking for participants, or you have already joined!", interaction)
